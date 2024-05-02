cbca3255506e0ad37c4177456449dda0d2e937ec
rikytestnet2 <shiro70229@gmail.com>
2024-05-02 21:44:59 +0700

    ffff

---
+++ application/views/front/modul/mod_facebook.php
@@ -0,0 +1,8 @@
+<div class="bs-callout bs-callout-primary">
+  <h4><i class="fa fa-search"></i> Instagram Page</h4>
+</div>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://www.instagram.com/know_photostudio/" nonce="NRs3YdmA"></script>
+<div class="fb-page" data-href="https://www.instagram.com/know_photostudio/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+  <blockquote cite="https://www.instagram.com/know_photostudio/" class="fb-xfbml-parse-ignore"><a href="https://www.instagram.com/know_photostudio/">know_photostudio</a></blockquote>
+</div>
